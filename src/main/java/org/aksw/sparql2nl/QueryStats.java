78b5d88a10a479f07ef8cff2f57e246e12cb6824
Jens Lehmann <jl@E6420.(none)>
2012-04-27 12:21:57 +0100

    created bug report for query statistics

--- src/main/java/org/aksw/sparql2nl/QueryStats.java
+++ src/main/java/org/aksw/sparql2nl/QueryStats.java
@@ -1,12 +1,17 @@
 package org.aksw.sparql2nl;
 
+import java.io.OutputStreamWriter;
 import java.util.Set;
+
+import javax.xml.transform.TransformerConfigurationException;
 
 import org.aksw.sparql2nl.queryprocessing.TriplePatternExtractor;
 import org.jgrapht.DirectedGraph;
 import org.jgrapht.alg.FloydWarshallShortestPaths;
+import org.jgrapht.ext.GraphMLExporter;
 import org.jgrapht.graph.DefaultDirectedGraph;
 import org.jgrapht.graph.DefaultEdge;
+import org.xml.sax.SAXException;
 
 import com.hp.hpl.jena.graph.Node;
 import com.hp.hpl.jena.graph.Triple;
@@ -17,8 +22,10 @@ public class QueryStats {
 
 	/**
 	 * @param args
+	 * @throws SAXException 
+	 * @throws TransformerConfigurationException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws TransformerConfigurationException, SAXException {
 
 		String queryString = "PREFIX dbo: <http://dbpedia.org/ontology/> "
 				+ "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "
@@ -34,10 +41,10 @@ public class QueryStats {
 				+ "PREFIX mo: <http://purl.org/ontology/mo/> "
 				+ "SELECT DISTINCT ?artisttype "
 				+ "WHERE {"
-				+ "?artist foaf:name 'Liz Story'."
+				+ "?artist foaf:name 'Liz Story'. ?artisttype rdf:subClassOf ?super . ?super rdf:type ?supsup ."
 				+ "?artist rdf:type ?artisttype ." + "}";
 
-		Query query = QueryFactory.create(queryString);
+		Query query = QueryFactory.create(queryString2);
 
 		System.out.println(query);
 
@@ -83,6 +90,20 @@ public class QueryStats {
 		System.out.println("in/out-degree: " + g.edgeSet().size()
 				/ (double) g.vertexSet().size());
 
+		GraphMLExporter<Node,DefaultEdge> ge = new GraphMLExporter();
+		ge.export(new OutputStreamWriter(System.out), g);
+		
+		// bug report (diameter 0 instead of 1)
+		DirectedGraph<String, DefaultEdge> graph = new DefaultDirectedGraph<String, DefaultEdge>(DefaultEdge.class);
+		String a = "a", b = "b", c = "c";
+		graph.addVertex(a);
+		graph.addVertex(b);
+		graph.addEdge(a, b);
+//		graph.addVertex(c);
+//		graph.addEdge(b, c);
+		FloydWarshallShortestPaths<String, DefaultEdge> fw = new FloydWarshallShortestPaths<String, DefaultEdge>(graph);
+//		System.out.println(fw.getDiameter());
+		
 	}
 
 }
