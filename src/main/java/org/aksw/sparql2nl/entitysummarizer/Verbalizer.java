3454aff50e262fec272a0ba9ed2653ebd5966518
Axel Ngonga <ngonga@informatik.uni-leipzig.de>
2013-06-11 23:04:37 +0200

    added subject merging (must be refined). Also implemented first version of verbalizer.

--- src/main/java/org/aksw/sparql2nl/entitysummarizer/Verbalizer.java
+++ src/main/java/org/aksw/sparql2nl/entitysummarizer/Verbalizer.java
@@ -8,13 +8,17 @@ import com.hp.hpl.jena.graph.Triple;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
+import org.aksw.sparql2nl.entitysummarizer.rules.ObjectMergeRule;
+import org.aksw.sparql2nl.entitysummarizer.rules.PredicateMergeRule;
+import org.aksw.sparql2nl.entitysummarizer.rules.Rule;
+import org.aksw.sparql2nl.entitysummarizer.rules.SubjectMergeRule;
 import org.aksw.sparql2nl.naturallanguagegeneration.SimpleNLGwithPostprocessing;
 import org.dllearner.kb.sparql.SparqlEndpoint;
 import simplenlg.framework.NLGElement;
 import simplenlg.phrasespec.SPhraseSpec;
 
 /**
- *
+ * A verbalizer for triples that all have the same subject.
  * @author ngonga
  */
 public class Verbalizer {
@@ -25,17 +29,42 @@ public class Verbalizer {
         nlg = new SimpleNLGwithPostprocessing(endpoint);
     }
     
-    public NLGElement verbalize(Set<Triple> triples)
+    /** Generates sentence for a given set of triples
+     * 
+     * @param triples A set of triples
+     * @return A set of sentences representing these triples
+     */
+    public List<NLGElement> verbalize(Set<Triple> triples)
     {
         List<SPhraseSpec> phrases = new ArrayList<SPhraseSpec>();
         for(Triple t: triples)
         {
             phrases.add(verbalize(t));
         }
-        return null;
+        int newSize = phrases.size(), oldSize = phrases.size()+1;
+        Rule mr = new ObjectMergeRule();
+        Rule pr = new PredicateMergeRule();
+        
+        //fix point iteration for object and predicate merging
+        while(newSize < oldSize)
+        {
+            oldSize = newSize;
+            int mrCount = mr.isApplicable(phrases);
+            int prCount = pr.isApplicable(phrases);
+            if(prCount > mrCount)
+                phrases = pr.apply(phrases);
+            else
+                phrases = mr.apply(phrases);
+            newSize = phrases.size();
+        }
+        return (new SubjectMergeRule()).apply(phrases);
     }
    
-    
+    /** Generates a simple phrase for a triple
+     * 
+     * @param triple A triple
+     * @return A simple phrases
+     */
     public SPhraseSpec verbalize(Triple triple)
     {
         return nlg.getNLForTriple(triple);
